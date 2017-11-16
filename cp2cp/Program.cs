0b905e8e5780fe4133d85595bd2a404599435609
kisssko <ya@kisssko.ru>
2017-11-16 23:51:36 +0300

    Now, it's work!

--- cp2cp/Program.cs
+++ cp2cp/Program.cs
@@ -11,11 +11,11 @@ namespace cp2cp
 	{
 		static string rowLine = new string('-', 77);
 		const string tplEncLine = "|{0,5}|{1,24}|{2,44}|";
-		const string tplU1 = " {0}: {1} <{2}> <{3}>";
+		const string tplU1 = " {0}: {1} <{2}> [<{3}>]";
 		const string tplU2 = " // {0}, {1}.";
 		const string PAC = "Press any key to continue . . . ";
-		const string msgSIn = "Читаем стандартный ввод";
-		const string msgSOut = "пишем в стандартный вывод";
+		const string msgSIn = "Программа читает стандартный ввод";
+		const string msgSOut = "и пишет в стандартный вывод";
 		const string msgUsage = "Использование";
 		const string msgSrcEnc = "Исходная кодировка";
 		const string msgDstEnc = "Целевая кодировка";
@@ -24,6 +24,10 @@ namespace cp2cp
 		const string msgDName = "Отображаемое имя";
 		const string msgInvSrcEnc = "Неверная входящая кодировка {0}!";
 		const string msgInvDstEnc = "Неверная исходящая кодировка {0}!";
+		const string msgEncFmt1 = " // Кодировку можно задать ";
+		const string msgEncFmt2 = "в виде кода или имени.";
+		const string msgEncInfo1 = " // Если стандартный вывод не перенаправлен -";
+		const string msgEncInfo2 = " // указывайе только исходную кодировку.";
 
 		static void Usage()
 		{
@@ -32,6 +36,9 @@ namespace cp2cp
 			                 );
 			Console.WriteLine(tplU1, msgUsage, exeName, msgSrcEnc, msgDstEnc);
 			Console.WriteLine(tplU2, msgSIn, msgSOut);
+			Console.WriteLine(msgEncFmt1 + msgEncFmt2);
+			Console.WriteLine(msgEncInfo1);
+			Console.WriteLine(msgEncInfo2);
 			Console.WriteLine(rowLine);
 			Console.WriteLine(tplEncLine, msgCode, msgName, msgDName);
 			Console.WriteLine(rowLine);
@@ -69,22 +76,36 @@ namespace cp2cp
 		public static int Main(string[] args)
 		{
 			int result = 0;
-			if (args.Length < 2) {
+			bool outRedir = Console.IsOutputRedirected;
+			Encoding srcEnc, dstEnc;
+			if (args.Length < 1 + (outRedir ? 1 : 0)) {
 				Usage();
 				WaitForKey();
 				return 0;
 			}
-			var srcEnc = GuessEncoding(args[0]);
-			var dstEnc = GuessEncoding(args[1]);
+			srcEnc = GuessEncoding(args[0]);
 			if (srcEnc == null) {
 				Console.WriteLine(msgInvSrcEnc, args[0]);
 				result |= 1;
 			}
-			if (dstEnc == null) {
-				Console.WriteLine(msgInvDstEnc, args[1]);
-				result |= 2;
+			
+			if (outRedir) {
+				dstEnc = GuessEncoding(args[1]);
+				if (dstEnc == null) {
+					Console.WriteLine(msgInvDstEnc, args[1]);
+					result |= 2;
+				} else {
+					Console.OutputEncoding = dstEnc;
+				}
 			}
-			WaitForKey();
+			if (result != 0)
+				return result;
+			
+			var stdin = new StreamReader(Console.OpenStandardInput(), srcEnc);
+			while (!stdin.EndOfStream)
+				Console.WriteLine(stdin.ReadLine());
+			
+			// WaitForKey();
 			return result;
 		}
 	}
